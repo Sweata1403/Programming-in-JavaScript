2f7c330ac1c8829e01c5d05ed8f0ec66d4182afe
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-10-08 13:06:07 +0530

    Add files via upload

---
+++ Video 4 Chai aur Code/variables.js
@@ -0,0 +1,19 @@
+const accountId=14453
+let accountEmail="sweata@google.com"
+var accountPassword="12345"
+acountCity="Jaipur"
+
+// accountId=2   (Not allowed)
+
+accountEmail="sweata12@google.com"
+accountPassword="54321"
+acountCity="Kolkata"
+
+//Longer version
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(acountCity);
+
+//Shorter version
+console.table([accountId, accountEmail, accountPassword, acountCity]);
