92206122895a18b76210c080e31d2bfd906e2ab4
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-10-09 18:36:07 +0530

    Add files via upload

---
+++ Video 5 Chai aur Code/datatypes.js
@@ -0,0 +1,43 @@
+"use strict";  //treat all js code as neaw version
+
+// alert(3+3) we are using nodejs, not browser
+
+// alert("Hello, World!"); // Displays a dialog with the message "Hello, World!"
+
+console.log(3+3)  
+
+console.log("Sweata");
+
+//Code readabilty should be high
+
+let name = "Sweata";  //String data type
+
+let age = 21; //Number datatype
+
+let loggedIn = true //boolean datatype
+
+
+//Therefore primitive types of datatypes are:
+//1. number => 2^53 is the range
+//2. bigint
+//3. string => " use it in double codes prefarably"
+//4. boolean => true or false
+//5. null => stand alone value
+//6. undefined => 
+//7. symbol => unique
+ 
+
+//Object datatypes:
+
+
+console.log(typeof "Sweata");
+
+
+console.log(typeof age);
+
+
+console.log(typeof null);
+
+
+
+console.log(typeof undefined);
